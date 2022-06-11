550261a5a7b4d8a5eb94e90eef9789447c1389d0
David Manriquez <dmanriqu@my.chemeketa.edu>
2022-06-11 07:25:18 -0700

    updated packages added key mgr file

--- commands/keys.js
+++ commands/keys.js
@@ -1,9 +1,22 @@
+const inquirer = require('inquirer');
+const colors = require('colors');
+const KeyManager = require ('../lib/KeyManger');
+
 const key = {
-    set () {
-        console.log('HODL set');
+   async set () {
+        const keyManager = new KeyManager();
+        const input = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'key',
+                message: 'Enter API Key'.green + 'https://nomics.com'
+            }
+        ]);
+
+        const key = keyManager.setKey(input.key);
     },
     show(){
-        console.log('HODl show');
+        console.log('HODl show'.blue);
     },
     remove(){
         console.log('HODl remove');

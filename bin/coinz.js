acc35147683dfe6d32ff3491452d3ad0f6d2cbf0
David Manriquez <dmanriqu@my.chemeketa.edu>
2022-06-11 06:42:07 -0700

    added file structure

--- bin/coinz.js
+++ bin/coinz.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node 
 
+//***  ***\\\
+const program = require('commander')
+const pkg = require('../package.json')
+
+program 
+ .version('1.0.0')
+ .command('key', 'Manage API Key --https://nomics.com')
+ .parse(process.argv);
+
 console.log('hello crypto fam')
+
---
+++ commands/keys.js
@@ -0,0 +1,13 @@
+const key = {
+    set () {
+        console.log('HODL set');
+    },
+    show(){
+        console.log('HODl show');
+    },
+    remove(){
+        console.log('HODl remove');
+    }
+}
+
+module .exports = key;
